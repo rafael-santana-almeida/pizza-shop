90d0fc298b53c9505a4aff90bdee8e1725198581
Rafael Santana <rafaelsantana.devweb@gmail.com>
2024-10-20 22:41:36 -0300

    Populando gráfico de Receitas no período e adicionando Skeletons nos componentes

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,9 +1,17 @@
-import { Link } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
 
 export function NotFound() {
+  const error = useRouteError() as Error
+
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-2">
-      <h1 className="text-4xl font-bold">Página não encontrada</h1>
+      <h1 className="text-4xl font-bold">
+        Whoops, algo inesperado aconteceu...
+      </h1>
+      <p className="text-accent-foreground">
+        Um erro aconteceu na aplicação, abaixo você encontra mais detalhes:
+      </p>
+      <pre>{error?.message || JSON.stringify(error)}</pre>
       <p className="text-accent-foreground">
         Voltar para o{' '}
         <Link to="/" className="text-sky-500 dark:text-sky-400">
--- src/routes.tsx
+++ src/routes.tsx
@@ -6,13 +6,14 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 import { NotFound } from './pages/not-found'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -25,5 +26,9 @@ export const router = createBrowserRouter([
       { path: '/sign-in', element: <SignIn /> },
       { path: '/sign-up', element: <SignUp /> },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
